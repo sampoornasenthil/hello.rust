07196ac4e20308077358a200a7319074f4d95667
sampoorna <sampoorna.senthil@gmail.com>
2018-06-29 20:26:07 +0530

    chapter two over,hell yeahgit status!

--- chapter2/lesson.rs
+++ chapter2/lesson.rs
@@ -6,6 +6,27 @@ println!("{} + {} = {}", 34, 80, 80 + 34);
 
 println!("{}", (30 - 5) % 5 + 20 * 30 / (3 + 2));
 
-print!("The sum is {}", 80.3 + 34.8);
+println!("The sum is {}", 80.3 + 34.8);
+println!("The sum is {}", 80.3 + 34.9);
+println!("{}", (23. - 6.) % 5. + 20. * 30. / (3. + 4.));
+println!("{}", 2.7 + 1.);
+println!("{} {}", -12 % 10, -1.2 % 1.);
+
+print!("{} + ", 80);
+print!("{} =", 34);
+println!(" {}", 80 + 34);
+ 
+
+    println!("{}", "This \
+        is \
+        just one line");
+  
+println!("{}", "These\n\
+    are\n\
+    three lines");
+
+
 
 }
+
+
