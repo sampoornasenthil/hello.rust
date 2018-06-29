43573d6708180afb165e8357eb18b5c0633e23d8
sampoorna <sampoorna.senthil@gmail.com>
2018-06-29 20:07:05 +0530

    arithmetic on the go

--- chapter2/lesson.rs
+++ chapter2/lesson.rs
@@ -1,5 +1,11 @@
 fn main(){
 print!("Hello, sam!\n");
 
+println!("The sum is {}.", 3459 + 4859);
+println!("{} + {} = {}", 34, 80, 80 + 34);
+
+println!("{}", (30 - 5) % 5 + 20 * 30 / (3 + 2));
+
+print!("The sum is {}", 80.3 + 34.8);
 
 }
