f1ba26608e06cbae49b6ceb159e3674b372d87c5
sampoorna <sampoorna.senthil@gmail.com>
2018-06-30 16:15:12 +0530

    chap3 till mut

---
+++ chapter3/naming.rs
@@ -0,0 +1,33 @@
+fn main(){   
+let number = 12;
+let other_number = 53;
+println!("{}", number + other_number);
+let number = 12;
+println!("{} {}", number, 47);
+ 
+let mut number = 12;
+println!("{}", number);
+number =53;
+println!(" {}", number);
+
+let number;
+number = 12;
+println!("{}", number);
+
+// equality of value
+let number1;
+let number2 = 22;
+number1 = number2;
+println!("{}", number1);
+
+
+
+
+
+
+
+
+
+
+
+}
